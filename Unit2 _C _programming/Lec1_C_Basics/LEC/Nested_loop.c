2a9f85ac2a3579e6d348a9f0a947fa81182028b0
Mario ADEL <madel2162@gmail.com>
2022-10-13 14:37:59 +0200

    add Unit2_C_programming

---
+++ Unit2 _C _programming/Lec1_C_Basics/LEC/Nested_loop.c
@@ -0,0 +1,23 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 13, 2022
+ *      Author: Mario_Adel
+ */
+
+#include <stdio.h>
+int main (void)
+{
+	int j,i;
+	for (j=0 ; j<=9 ; j++)
+	{
+		for( i=j ; i<=9 ; i++)
+		{
+			printf("%d ",i);
+
+		}
+		printf("\n");
+	}
+
+}
+
